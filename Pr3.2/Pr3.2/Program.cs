9de338a5a4d9b4d2c4bc7f844a8673d56cfc13e2
MeefiTyan <117981541+MeefiTyan@users.noreply.github.com>
2024-03-20 14:47:05 +0200

    Update Program.cs

--- Pr3.2/Pr3.2/Program.cs
+++ Pr3.2/Pr3.2/Program.cs
@@ -1,55 +1,5 @@
 ﻿using System;
 
-class Point
-{
-    public int X { get; }
-    public int Y { get; }
-    public string Name { get; }
-
-    public Point(int x, int y, string name)
-    {
-        X = x;
-        Y = y;
-        Name = name;
-    }
-}
-
-class Figure
-{
-    private Point[] points;
-
-    public Figure(Point A, Point B, Point C)
-    {
-        points = new Point[] { A, B, C };
-    }
-
-    public Figure(Point A, Point B, Point C, Point D)
-    {
-        points = new Point[] { A, B, C, D };
-    }
-
-    public Figure(Point A, Point B, Point C, Point D, Point E)
-    {
-        points = new Point[] { A, B, C, D, E };
-    }
-
-    private double LengthSide(Point A, Point B)
-    {
-        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
-    }
-
-    public void PerimeterCalculator()
-    {
-        double perimeter = 0;
-        for (int i = 0; i < points.Length - 1; i++)
-        {
-            perimeter += LengthSide(points[i], points[i + 1]);
-        }
-        perimeter += LengthSide(points[points.Length - 1], points[0]);
-        Console.WriteLine($"Perimeter: {perimeter}");
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
